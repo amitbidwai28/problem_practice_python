9bcdd9db00f8aeb2f9f53cfab903de5dda4a1c7e
amitbidwai28 <amitbidwai281@gmail.com>
2024-07-14 21:56:47 +0530

    commited

--- Python_DS_Programs/list_ascending.py
+++ Python_DS_Programs/list_ascending.py
@@ -1,6 +1,7 @@
 # Initialize array
 arr = [5, 2, 8, 7, 1]
-temp: int = 0
+temp = 0
+arr_temp = []
 
 # Displaying elements of original array
 print("Elements of original array: ")
@@ -10,15 +11,17 @@ for i in range(0, len(arr)):
 # Sort the array in ascending order
 for i in range(0, len(arr)):
     for j in range(i + 1, len(arr)):
-        if (arr[i] > arr[j]):
+        if  arr[i] > arr[j]:
             temp = arr[i]
             arr[i] = arr[j]
             arr[j] = temp
-
+    arr_temp.append(arr[i])
 print()
+print("Elements of array in list format: ")
+print(arr_temp)
 
 # Displaying elements of the array after sorting
 
-print("Elements of array sorted in ascending order: ")
-for i in range(0, len(arr)):
-    print(arr[i], end=" ")
+print("Elements of array sorted in ascending order without list format: ")
+for i in arr_temp:
+    print(i, end=" ")
