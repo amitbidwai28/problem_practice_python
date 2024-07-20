715ae9e767a275c0bd01d80eceb4fb0a417f038e
amitbidwai28 <amitbidwai281@gmail.com>
2024-07-20 10:06:20 +0530

    commited

--- Python_String/forth_last_for.py
+++ Python_String/forth_last_for.py
@@ -1,15 +1,11 @@
-
 def forth_last_char(string):
     if len(string) <= 4:
         return "String is too short to count the character"
 
     n = len(string)
     for ele in range(n):
-        if ele == n-4:
+        if ele == n - 4:
             return string[ele]
-
-
-
 
 
 forth_char = input("Enter the string: ")
