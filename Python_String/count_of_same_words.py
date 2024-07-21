1d2f20c02a1ba2a0fad0181aefbc9a9f481d8942
amitbidwai28 <amitbidwai281@gmail.com>
2024-07-21 19:27:16 +0530

    'commited'

---
+++ Python_String/count_of_same_words.py
@@ -0,0 +1,16 @@
+def count_words_regex(input_string, search_string):
+    x = search_string
+    temp = 0
+    # Use regular expression to find all words
+    words = input_string.split()
+    for eachwords in words:
+        if eachwords == x:
+            temp = temp + 1
+
+    return temp
+
+
+# Example usage:
+input_string = input("Enter the string: ")
+seacrh_string = input("Enter the string which you want to search: ")
+print("Count of the string", seacrh_string, "is: ", count_words_regex(input_string, seacrh_string))
